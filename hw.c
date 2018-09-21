e610a367f96f3039ab81209391759eb8855c2b15
KinvenD <37690427+KinvenD@users.noreply.github.com>
2018-09-21 22:48:37 +0700

    Update hw.c

--- hw.c
+++ hw.c
@@ -1,11 +1,11 @@
 #include <stdio.h> 
 
 int main() { 
-char yee[30]; 
-if (scanf("%s", yee)!=EOF){ 
-printf("Hello, %s!", yee); 
-}else{ 
-printf("Hello, nameless!"); 
-} 
-return 0; 
+  char yee[30]; 
+  if (scanf("%s", yee) != EOF){ 
+    printf("Hello, %s!", yee); 
+  }else{ 
+    printf("Hello, nameless!"); 
+  } 
+  return 0; 
 }
